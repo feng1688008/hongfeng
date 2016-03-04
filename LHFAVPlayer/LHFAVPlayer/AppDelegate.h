d34aad74f7d0d65d6ba32abf95f5a026f728aea5
LHF <16420330@qq.com>
2016-03-04 09:42:22 +0800

    First blood

---
+++ LHFAVPlayer/LHFAVPlayer/AppDelegate.h
@@ -0,0 +1,17 @@
+//
+//  AppDelegate.h
+//  LHFAVPlayer
+//
+//  Created by 李洪峰 on 16/1/28.
+//  Copyright © 2016年 LHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+
+@end
+
---
+++ LHFAVPlayer/LHFAVPlayer/ViewController.h
@@ -0,0 +1,15 @@
+//
+//  ViewController.h
+//  LHFAVPlayer
+//
+//  Created by 李洪峰 on 16/1/28.
+//  Copyright © 2016年 LHF. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface ViewController : UIViewController
+
+
+@end
+
